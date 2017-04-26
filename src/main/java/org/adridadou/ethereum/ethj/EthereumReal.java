b0ac62586470ce8a678c7892bf12126e24c247db
adridadou <david@roon.me>
2017-04-26 22:34:51 +0200

    ethj release !!

--- src/main/java/org/adridadou/ethereum/ethj/EthereumReal.java
+++ src/main/java/org/adridadou/ethereum/ethj/EthereumReal.java
@@ -1,8 +1,10 @@
 package org.adridadou.ethereum.ethj;
 
 import org.adridadou.ethereum.propeller.EthereumBackend;
+import org.adridadou.ethereum.propeller.event.BlockInfo;
 import org.adridadou.ethereum.propeller.event.EthereumEventHandler;
 import org.adridadou.ethereum.propeller.values.*;
+import org.ethereum.core.Block;
 import org.ethereum.core.BlockchainImpl;
 import org.ethereum.core.Repository;
 import org.ethereum.core.Transaction;
@@ -10,6 +12,8 @@ import org.ethereum.crypto.ECKey;
 import org.ethereum.facade.Ethereum;
 
 import java.math.BigInteger;
+import java.util.Collections;
+import java.util.stream.Collectors;
 
 import static org.adridadou.ethereum.propeller.values.EthValue.wei;
 
@@ -61,6 +65,16 @@ public class EthereumReal implements EthereumBackend {
     }
 
     @Override
+    public BlockInfo getBlock(long blockNumber) {
+        return toBlockInfo(ethereum.getBlockchain().getBlockByNumber(blockNumber));
+    }
+
+    @Override
+    public BlockInfo getBlock(EthHash ethHash) {
+        return toBlockInfo(ethereum.getBlockchain().getBlockByHash(ethHash.data));
+    }
+
+    @Override
     public SmartContractByteCode getCode(EthAddress address) {
         return SmartContractByteCode.of(getRepository().getCode(address.address));
     }
@@ -90,5 +104,13 @@ public class EthereumReal implements EthereumBackend {
 
     private Repository getRepository() {
         return getBlockchain().getRepository();
+    }
+
+    private BlockInfo toBlockInfo(Block block) {
+        return new BlockInfo(block.getNumber(), block.getTransactionsList().stream().map(this::toReceipt).collect(Collectors.toList()));
+    }
+
+    private org.adridadou.ethereum.propeller.event.TransactionReceipt toReceipt(Transaction tx) {
+        return new org.adridadou.ethereum.propeller.event.TransactionReceipt(EthHash.of(tx.getHash()), EthAddress.of(tx.getSender()), EthAddress.of(tx.getReceiveAddress()), EthAddress.empty(), "", EthData.empty(), true, Collections.emptyList());
     }
 }
