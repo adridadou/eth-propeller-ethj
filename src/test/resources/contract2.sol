3f043d35ffe0c894b98c4bbed11de8fb58a44f6e
adridadou <david@roon.me>
2017-10-30 17:16:33 +0100

    fixed some issues while testing

--- src/test/resources/contract2.sol
+++ src/test/resources/contract2.sol
@@ -4,25 +4,25 @@ pragma solidity ^0.4.6;
 contract myContract2 {
     address owner;
 
-    function getI1() constant returns (string) {return "hello";}
+    function getI1() public pure returns (string) {return "hello";}
 
-    function getT() constant returns (bool) {return true;}
+    function getT() public pure returns (bool) {return true;}
 
-    function getM() constant returns (bool, string, uint) {return (true, "hello", 34);}
+    function getM() public pure returns (bool, string, uint) {return (true, "hello", 34);}
 
-    function getArray() constant returns (uint[10] arr) {
+    function getArray() public pure returns (uint[10] arr) {
         for (uint i = 0; i < 10; i++) {
             arr[i] = i;
         }
     }
 
-    function getArray2() constant returns (uint[10] arr) {
+    function getArray2() public pure returns (uint[10] arr) {
         for (uint i = 0; i < 10; i++) {
             arr[i] = i;
         }
     }
 
-    function getOwner() constant returns (address) {
+    function getOwner() public constant returns (address) {
         return owner;
     }
 }
